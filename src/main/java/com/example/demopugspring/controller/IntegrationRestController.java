9a0f43c6ad12bf655886dfe1c383e4aba2367610
Alexandre Santos <alexandre.santos@jmellosaude.pt>
2020-10-27 18:24:14 +0000

    return text/plain in rest controller or exception

--- src/main/java/com/example/demopugspring/controller/IntegrationRestController.java
+++ src/main/java/com/example/demopugspring/controller/IntegrationRestController.java
@@ -1,6 +1,7 @@
 package com.example.demopugspring.controller;
 
 import com.example.demopugspring.engine.MapperEngine;
+import com.example.demopugspring.engine.MapperError;
 import com.example.demopugspring.engine.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,11 +15,21 @@ public class IntegrationRestController {
     private static final Logger log = LoggerFactory.getLogger(IntegrationRestController.class);
     @Autowired
     MapperEngine mapperEngine;
-    @PostMapping(value = "/mapper", consumes = "text/plain", produces = "application/json")
-    public Response map(@RequestBody String msg) {
-        Response response = new Response();
+    @PostMapping(value = "/mapper", consumes = "text/plain", produces = "text/plain")
+    public String map(@RequestBody String msg) throws Exception {
+        Response response;
         response = mapperEngine.run(msg);
-        return response;
+        if (response.getErrorList().size() > 0) {
+            StringBuilder errorText = new StringBuilder("Found ");
+            errorText.append(response.getErrorList().size()).append(" error(s).\n");
+            for (MapperError error: response.getErrorList()) {
+                errorText.append(error.getField())
+                        .append("::")
+                        .append(error.getError())
+                        .append("\n");
+            }
+            throw new Exception(errorText.toString());
+        }
+        return response.getMessage();
     }
-
 }
