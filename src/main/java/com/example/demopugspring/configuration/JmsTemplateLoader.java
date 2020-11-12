4036a2983547d499b8d2251f720f3cc24720a84a
lfcmaia <liliana.fc.maia@jmellosaude.pt>
2020-11-12 09:42:03 +0000

    add a new TemplateLoader to get File

---
+++ src/main/java/com/example/demopugspring/configuration/JmsTemplateLoader.java
@@ -0,0 +1,110 @@
+package com.example.demopugspring.configuration;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.file.Paths;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+import de.neuland.pug4j.template.TemplateLoader;
+
+public class JmsTemplateLoader implements TemplateLoader, ResourceLoaderAware {
+
+	private ResourceLoader resourceLoader;
+	private String encoding = "UTF-8";
+	private String suffix = ".pug";
+	private String templateLoaderPath = "";
+	private String base = "";
+	private boolean searchFile;
+
+	public void setSearchFile(boolean searchFile) {
+		this.searchFile = searchFile;
+	}
+
+	@Override
+	public long getLastModified(String name) {
+		Resource resource = getResource(name);
+		try {
+			return resource.lastModified();
+		} catch (IOException ex) {
+			return -1;
+		}
+	}
+
+	@Override
+	public Reader getReader(String name) throws IOException {
+		Resource resource = getResource(name);
+		return new InputStreamReader(resource.getInputStream(), encoding);
+	}
+
+	@Override
+	public String getExtension() {
+		return suffix.substring(1, suffix.length());
+	}
+
+	private Resource getResource(String name) {
+		String resourceName = getResourceName(name);
+		if (hasNoExtension(resourceName)) {
+			resourceName += suffix;
+		}
+		if (searchFile) {
+			return this.resourceLoader.getResource("file:" + resourceName);
+		} else
+			return this.resourceLoader.getResource(resourceName);
+	}
+
+	private String getResourceName(String name) {
+		if (!StringUtils.isBlank(templateLoaderPath))
+			if (Paths.get(name).isAbsolute()) {
+				return Paths.get(templateLoaderPath + base + name.substring(1)).toString();
+			} else
+				return Paths.get(templateLoaderPath).resolve(name).toString();
+		else
+			return name;
+	}
+
+	private boolean hasNoExtension(String filename) {
+		return "".equals(FilenameUtils.getExtension(filename));
+	}
+
+	public String getEncoding() {
+		return encoding;
+	}
+
+	public void setEncoding(String encoding) {
+		this.encoding = encoding;
+	}
+
+	public String getSuffix() {
+		return suffix;
+	}
+
+	public void setSuffix(String suffix) {
+		this.suffix = suffix;
+	}
+
+	public String getBase() {
+		return base;
+	}
+
+	public void setBase(String base) {
+		this.base = base;
+	}
+
+	public void setTemplateLoaderPath(String templateLoaderPath) {
+		if (templateLoaderPath.endsWith("\\"))
+			this.templateLoaderPath = templateLoaderPath;
+		else
+			this.templateLoaderPath = templateLoaderPath + "\\";
+	}
+
+	@Override
+	public void setResourceLoader(ResourceLoader resourceLoader) {
+		this.resourceLoader = resourceLoader;
+	}
+}
--- src/main/java/com/example/demopugspring/configuration/PugConfig.java
+++ src/main/java/com/example/demopugspring/configuration/PugConfig.java
@@ -1,37 +1,41 @@
 package com.example.demopugspring.configuration;
 
-import de.neuland.pug4j.PugConfiguration;
-import de.neuland.pug4j.spring.template.SpringTemplateLoader;
-import de.neuland.pug4j.spring.view.PugViewResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 
+import de.neuland.pug4j.PugConfiguration;
+import de.neuland.pug4j.spring.view.PugViewResolver;
+import de.neuland.pug4j.template.TemplateLoader;
+
 @Configuration
 public class PugConfig {
+	private static boolean ISWINDOWS = false;
 
-    @Bean
-    public SpringTemplateLoader templateLoader() {
-        SpringTemplateLoader templateLoader = new SpringTemplateLoader();
-        templateLoader.setTemplateLoaderPath("classpath:/templates/");
-        templateLoader.setEncoding("UTF-8");
-        templateLoader.setSuffix(".pug");
-        return templateLoader;
-    }
+	@Bean
+	public TemplateLoader templateLoader() {
+		JmsTemplateLoader templateLoader = new JmsTemplateLoader();
+		// templateLoader.setTemplateLoaderPath(Paths.get("./src/main/resources/templates/").toAbsolutePath().toString());
+		templateLoader.setTemplateLoaderPath("classpath:/templates/");
+		templateLoader.setSearchFile(ISWINDOWS);
+		templateLoader.setEncoding("UTF-8");
+		templateLoader.setSuffix(".pug");
+		return templateLoader;
+	}
 
-    @Bean
-    public PugConfiguration pugConfiguration() {
-        PugConfiguration configuration = new PugConfiguration();
-        configuration.setCaching(false);
-        configuration.setTemplateLoader(templateLoader());
-        configuration.setPrettyPrint(true);
-        return configuration;
-    }
+	@Bean
+	public PugConfiguration pugConfiguration() {
+		PugConfiguration configuration = new PugConfiguration();
+		configuration.setCaching(false);
+		configuration.setTemplateLoader(templateLoader());
+		configuration.setPrettyPrint(true);
+		return configuration;
+	}
 
-    @Bean
-    public ViewResolver viewResolver() {
-        PugViewResolver viewResolver = new PugViewResolver();
-        viewResolver.setConfiguration(pugConfiguration());
-        return viewResolver;
-    }
+	@Bean
+	public ViewResolver viewResolver() {
+		PugViewResolver viewResolver = new PugViewResolver();
+		viewResolver.setConfiguration(pugConfiguration());
+		return viewResolver;
+	}
 }
