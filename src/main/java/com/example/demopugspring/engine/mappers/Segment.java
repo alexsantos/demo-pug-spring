586a19df1a1243fbe37dc1d4fbda9d9fab8950ca
luis.torres.costa <luis.torres.costa@jmellosaude.pt>
2020-11-18 15:03:56 +0000

    Added support for Numeric & Segment:
    - Created Numeric & Segment mapper classes.

---
+++ src/main/java/com/example/demopugspring/engine/mappers/Segment.java
@@ -0,0 +1,41 @@
+package com.example.demopugspring.engine.mappers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.demopugspring.engine.MapperEngine;
+import com.example.demopugspring.engine.MapperError;
+
+import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.model.Message;
+import ca.uhn.hl7v2.util.Terser;
+
+public class Segment extends AbstractMapper {
+
+	// TODO ADD MAPPER DESCRIPTION
+	private static final String MAPPER_DESCRIPTION = "";
+
+	public Segment(MapperEngine engine, Message incomingMessage, Message outgoingMessage, Terser incomingTerser, Terser outgoingTerser, List<String> keys, String value) {
+		super(engine, incomingMessage, outgoingMessage, incomingTerser, outgoingTerser, keys, value);
+	}
+
+	@Override
+	public String getDescription() {
+		return MAPPER_DESCRIPTION;
+	}
+
+	@Override
+	public List<MapperError> mapKey(String key) {
+		ArrayList<MapperError> errors = new ArrayList<MapperError>();
+
+		try {
+			// TODO VALIDATE THIS MAPPER BEHAVIOUR
+			outgoingTerser.getSegment(key).parse(key);
+		} catch (HL7Exception e) {
+			log.error(e.getMessage());
+			errors.add(new MapperError(key, e.getMessage()));
+		}
+
+		return errors;
+	}
+}
