cb32afe45fe8f0693534a7daab0bf8664214d02a
luis.torres.costa <luis.torres.costa@jmellosaude.pt>
2020-11-26 18:31:37 +0000

    RestController returning Response model

--- src/main/java/com/example/demopugspring/controller/IntegrationRestController.java
+++ src/main/java/com/example/demopugspring/controller/IntegrationRestController.java
@@ -1,14 +1,17 @@
 package com.example.demopugspring.controller;
 
-import com.example.demopugspring.engine.MapperEngine;
-import com.example.demopugspring.engine.MapperError;
-import com.example.demopugspring.engine.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demopugspring.engine.MapperEngine;
+import com.example.demopugspring.engine.Response;
 
 @RestController
 public class IntegrationRestController {
@@ -16,21 +19,10 @@ public class IntegrationRestController {
 	@Autowired
 	MapperEngine mapperEngine;
 
-	@PostMapping(value = "/mapper", consumes = "text/plain", produces = "text/plain")
-	public String map(@RequestBody String msg) throws Exception {
-		Response response;
-		response = mapperEngine.run(msg);
-		if (response.getErrorList().size() > 0) {
-			StringBuilder errorText = new StringBuilder("Found ");
-			errorText.append(response.getErrorList().size()).append(" error(s).\n");
-			for (MapperError error : response.getErrorList()) {
-				errorText.append(error.getField())
-						.append("::")
-						.append(error.getError())
-						.append("\n");
-			}
-			throw new Exception(errorText.toString());
-		}
-		return response.getMessage();
+	@PostMapping(value = "/mapper", consumes = "text/plain", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> map(@RequestBody String msg) {
+		Response response = mapperEngine.run(msg);
+		return response.getErrorList().isEmpty() ? new ResponseEntity<>(response, HttpStatus.OK)
+				: new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
 	}
 }
